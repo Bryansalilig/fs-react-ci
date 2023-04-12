18f642a351373163fb87723810d621c031172e5c
Bryan Salilig <bryan.salilig@mobilemoney.ph>
2023-04-12 15:30:34 +0800

    Set up Model and Controller

--- backend/app/Database/Migrations/2023-04-12-070958_Products.php
+++ backend/app/Database/Migrations/2023-04-12-070958_Products.php
@@ -8,7 +8,24 @@ class Products extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ],
+            'price' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('products');
     }
 
     public function down()
